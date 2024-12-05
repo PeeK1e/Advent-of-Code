3815c2df55ad16cb5276701656d3c6778c617776
Ryuko <ryuko@peek1e.eu>
2024-12-05 21:12:39 +0100

    5 days of abominations

--- 2024/4/src/main.rs
+++ 2024/4/src/main.rs
@@ -1,163 +1,17 @@
-use regex::Regex;
+mod solve;
+
+use solve::*;
 
 fn main() {
     let content = std::fs::read_to_string("./input").unwrap();
 
-    let one: i32 = solve_t1(&content);
+    let one: i32 = solve:: solve_t1(&content);
     println!("{one}");
 
     let two: i32 = solve_t2(&content);
     println!("{two}");
 }
 
-fn solve_t1(content: &str) -> i32 {
-    let strings = make_strings(content);
-    let rgx1 = Regex::new(r"(XMAS)").unwrap();
-    let rgx2 = Regex::new(r"(SAMX)").unwrap();
-
-    let mut count:i32 = 0;
-    for s in &strings {
-        rgx1.captures_iter(s).for_each(|_| count+=1 );
-    }
-    for s in &strings {
-        rgx2.captures_iter(s).for_each(|_| count+=1 );
-    }
-
-    count
-}
-
-fn solve_t2(content: &str) -> i32 {
-    let map = make_2d_vec(content);
-
-    let mut count = 0;
-    // only 1..n-1
-    let row_len = map.get(0).unwrap().len();
-    for y in 1..map.len()-1 {
-        // only 1..n-1
-        for x in 1..row_len-1 {
-            if *map.get(y).unwrap().get(x).unwrap() == 'A' {
-                count += search_mas(&map, x, y);
-            }
-        }
-    }
-
-    return count;
-}
-
-fn search_mas(map: &Vec<Vec<char>>, x: usize, y: usize) -> i32 {
-    let tl = *map.get(y-1).unwrap().get(x-1).unwrap();
-    let br = *map.get(y+1).unwrap().get(x+1).unwrap();
-
-    let tr = *map.get(y-1).unwrap().get(x+1).unwrap();
-    let bl = *map.get(y+1).unwrap().get(x-1).unwrap();
-
-    let mut diagright = false; 
-    let mut diagleft = false; 
-    if tl == 'S' && br == 'M' || tl == 'M' && br == 'S' {
-        diagright = true;
-    }
-    if bl == 'S' && tr == 'M' || bl == 'M' && tr == 'S' {
-        diagleft = true;
-    }
-
-    if diagleft && diagright {
-        return 1;
-    }
-
-    return 0;
-}
-
-fn make_strings(input: &str) -> Vec<String> {
-    let mut strings = vec![];
-    //horizontal
-    input.lines().for_each(|l| strings.push(l.to_string()));
-    
-    let map = make_2d_vec(input);
-    let len_row = map.get(0).unwrap().len();
-    let len_cols = map.len();
-
-    // vertically
-    for x in 0..len_row as usize {
-        let mut word = String::from("");
-        for y in 0..len_cols {
-            let character = map.get(y).unwrap().get(x).unwrap();
-            word.push(character.clone());
-        }
-        strings.push(word);
-    }
-
-    // diagonally right
-    for x in 0..len_row as usize {
-        let mut word = String::from("");
-        for y in 0..len_cols {
-            if x + y > len_row-1 {
-                break;
-            }
-            let character = map.get(y).unwrap().get(x+y).unwrap();
-            word.push(character.clone());
-        }
-        strings.push(word);
-    }
-
-    // diagonally left
-    for x in 0..len_row as usize {
-        let mut word = String::from("");
-        for y in 0..len_cols {
-            let idx = len_row - 1 - x;
-            let character = map.get(y).unwrap().get(idx-y).unwrap();
-            word.push(character.clone());
-            if idx-y == 0 {
-                break;
-            }
-        }
-        strings.push(word);
-    }
-    
-    // diagonally right y = 1
-    for y in 1..len_cols {
-        let mut word = String::from("");
-        for x in 0..len_row as usize {
-            if x + y > len_cols-1 {
-                break;
-            }
-            let character = map.get(x+y).unwrap().get(x).unwrap();
-            word.push(character.clone());
-        }
-        strings.push(word);
-    }
-
-    // diagonally left y = 1
-    for y in 1..len_cols {
-        let mut word = String::from("");
-        for x in 0..len_row as usize {
-            if x + y > len_cols-1 {
-                break;
-            }
-            let idx = len_row - 1 - x;  
-            let character = map.get(x+y).unwrap().get(idx).unwrap();
-            word.push(character.clone());
-        }
-        strings.push(word);
-    }
-    
-    return strings;
-}
-
-fn make_2d_vec(input: &str) -> Vec<Vec<char>>{
-    input
-        .lines()
-        .map(|l| {
-            let mut items = vec![];
-            for e in l.as_bytes() {
-                items.push(*e as char);
-            }
-            items
-    }).collect()
-}
-
-#[cfg(test)]
-mod test {
-    use crate::{solve_t1, solve_t2};
 
     static INPUT: &str = "MMMSXXMASM
 MSAMXMSMSA
@@ -170,17 +24,17 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-    #[test]
-    fn sample_t1() {
-        let res = solve_t1(&INPUT);
+#[test]
+fn sample_t1() {
+    let res = solve_t1(&INPUT);
 
-        assert_eq!(18, res);
-    }
-
-    #[test]
-    fn sample_t2() {
-        let res = solve_t2(&INPUT);
-
-        assert_eq!(9, res);
-    }
+    assert_eq!(18, res);
 }
+
+#[test]
+fn sample_t2() {
+    let res = solve_t2(&INPUT);
+
+    assert_eq!(9, res);
+}
+
--- 2024/5/src/main.rs
+++ 2024/5/src/main.rs
@@ -35,15 +35,14 @@ mod test {
     fn sample_t1() {
         let res = solve_t1(&INPUT);
 
-        assert_eq!(0, res);
-        // assert_eq!(143, res);
+        assert_eq!(143, res);
     }
 
     #[test]
     fn sample_t2() {
         let res = solve_t2(&INPUT);
 
-        assert_eq!(9, res);
+        assert_eq!(123, res);
     }
 }
 
@@ -63,9 +62,7 @@ fn solve_t1(content: &str) -> i32 {
     let (map, order) = make_input_maps(&content);
 
     for o in order {
-        for v in o.split(",") {
-            
-        }
+        count += instruction_contains_ruleset(&map, o);
     }
 
     return count;
@@ -73,8 +70,85 @@ fn solve_t1(content: &str) -> i32 {
 
 fn solve_t2(content: &str) -> i32 {
     let mut count = 0;
+    let (map, order) = make_input_maps(&content);
+
+    for o in order {
+        count += instruction_contains_ruleset_try_correct(&map, o, 0, false);
+    }
 
     return count;
+}
+
+fn instruction_contains_ruleset(rules: &HashMap<String, Vec<&str>>, instructions: &str) -> i32 {
+    let inst: Vec<&str> = instructions.split(",").collect();
+    let len = inst.len();
+
+    for i in (0..len).rev() {
+        let key = inst.get(i).unwrap().to_string();
+        match rules.get(&key) {
+            Some(rule) => {
+                let (b, _) = check_rule(rule, &inst, i);
+                if b {
+                    continue;
+                } else {
+                    return 0;
+                }
+            }
+            None => continue
+        }
+    }
+    let idx = (len-1) / 2;
+    return inst.get(idx).unwrap().parse::<i32>().unwrap();
+}
+
+fn instruction_contains_ruleset_try_correct(rules: &HashMap<String, Vec<&str>>, instructions: &str, count: i32, corrected: bool) -> i32 {
+    let mut inst: Vec<&str> = instructions.split(",").collect();
+    let len = inst.len();
+
+    if count > (len*len) as i32 {
+        return 0;
+    }
+
+    for i in (0..len).rev() {
+        let key = inst.get(i).unwrap().to_string();
+        match rules.get(&key) {
+            Some(rule) => {
+                let (b,j) = check_rule(rule, &inst, i);
+                if !b {
+                    inst.swap(i, j);
+                    let string = inst.join(",");
+                    return instruction_contains_ruleset_try_correct(rules, &string, count+1, true);
+                } else {
+                    continue;
+                }
+            }
+            None => {
+                continue;
+            }
+        }
+    }
+    if !corrected {
+        return 0;
+    }
+    let idx = (len-1) / 2;
+    return inst.get(idx).unwrap().parse::<i32>().unwrap();
+}
+
+
+fn check_rule<'a>(rule: &Vec<&str>, instructions: &Vec<&str>, idx_limit: usize) -> (bool, usize){
+    for r in rule {
+        let mut c = 0;
+        for i in instructions {
+            if c == idx_limit {
+                continue;
+            }
+            if r == i {
+                return (false, c);
+            }
+            c += 1;
+        }
+    }
+    return (true, 0);
 }
 
 fn make_input_maps(content: &str) -> (HashMap<String, Vec<&str>>,Vec<&str>) {
