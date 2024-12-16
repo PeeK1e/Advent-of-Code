13938192ddb3d63ee793e55fd6f17ab7cd4a864c
Ryuko <ryuko@peek1e.eu>
2024-12-16 16:04:51 +0100

    16 bench update lol

--- 2024/16/benches/benches.rs
+++ 2024/16/benches/benches.rs
@@ -5,12 +5,18 @@ use solution::solve::{solve_t1, solve_t2};
 
 fn bench_part_1(c: &mut Criterion) {
     let content = std::fs::read_to_string("./input").unwrap();
-    c.bench_function("part_1", |b| b.iter(|| solve_t1(&content)));
+    let mut group = c.benchmark_group("solve");
+    group.measurement_time(Duration::new(30, 0));
+    group.bench_function("part_1", |b| b.iter(|| solve_t1(&content)));
+    group.finish();
 }
 
 fn bench_part_2(c: &mut Criterion) {
     let content = std::fs::read_to_string("./input").unwrap();
-    c.bench_function("part_2", |b| b.iter(|| solve_t2(&content)));
+    let mut group = c.benchmark_group("solve");
+    group.measurement_time(Duration::new(30, 0));
+    group.bench_function("part_2", |b| b.iter(|| solve_t2(&content)));
+    group.finish();
 }
 
 criterion_group!(benches, bench_part_1, bench_part_2);
